05c5c34789a5a97c4e2e6be587d8d35e0e6cc115
Peter Victoratos <pvictoratos@elon.edu>
2020-01-29 13:49:19 -0500

    formatted Managed Object Model lab

--- eBooks/eBooks Model/eBooksModelTests/Obj-C Tests/ManagedObjectModelTests.m
+++ eBooks/eBooks Model/eBooksModelTests/Obj-C Tests/ManagedObjectModelTests.m
@@ -11,24 +11,19 @@
 
 @import eBooksModel;
 
-static NSString * const pathURLString = @"/Users/pvictoratos/Documents/GitHub/advanced-ios/Projects/eBooks/eBooks Model/eBooksModel/eBooks.xcdatamodeld";
-
 @interface ManagedObjectModelTests : XCTestCase
-@property (strong, nonatomic)NSManagedObjectModel *model;
-@property (weak, nonatomic)NSAttributeDescription *attribute;
-@property (copy, nonatomic) NSURL *modelURL;
-@property (copy, nonatomic) NSString *path;
+@property (strong, nonatomic) NSManagedObjectModel *model;
+
 @end
 
 @implementation ManagedObjectModelTests
 
 - (void)setUp {
     //make the easy things simple and the hard things possible
-    
-    //mixture of the below code wrapped within a setUp method
+
     NSBundle *bundle = [NSBundle bundleForClass:ManagedObject.class];
-    NSString *path = [bundle pathForResource:@"eBooks" ofType:@"momd"];
-    self.modelURL = [[NSURL alloc] initFileURLWithPath:path];
+    NSURL *modelURL = [bundle URLForResource:@"eBooks" withExtension:@"momd"];
+    self.model = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
 }
 
 - (void)testStuff {
@@ -38,7 +33,7 @@ static NSString * const pathURLString = @"/Users/pvictoratos/Documents/GitHub/ad
     printf("%d, %p, %p\n", x, y, z);
 }
 
-- (void)testBundlePath{
+- (void)testBundlePath {
     //gets app directory bundle on whatever system you run the app on
     //NSLog(@"%@", NSBundle.mainBundle);
     //gets bundle directory of the testing framework
@@ -47,63 +42,57 @@ static NSString * const pathURLString = @"/Users/pvictoratos/Documents/GitHub/ad
     // getting main bundle in its current location to get the path for the instance of it
     NSBundle *bundle = [NSBundle bundleForClass:ManagedObject.class];
     NSString *path = [bundle pathForResource:@"eBooks" ofType:@"momd"];
-    NSLog(@"%@", path);
+    NSLog(@"Path is %@. Cool.", path);
 }
 
 - (void)testLoadModel {
     //Loads & populates model with frameowrk target's bundle
-    NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:self.modelURL];
-    NSLog(@"%@", model);
-}
-
-- (void)testEntityDescriptionsLoop {
-    //Loops through & organizes the different attributes of the bundle
-    NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:self.modelURL];
-    
-    for (NSEntityDescription *entity in model.entities) {
-        //What's the most basic way to do this?
-        //Print out one line at a time
-        
-        for (NSPropertyDescription *property in entity){
-            NSLog(@"Properties by name: %@", property);
-        }
-        //should print out Entity: [entity name] ________________
-        //another loop should print out attributes by name using key, attribute pair
-
-        //last loop should print out relationsihps by name
-
-    }
+    NSLog(@"%@", self.model);
 }
 
 - (void)testEntityNameFormat {
-    //NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:self.modelURL];
-    //NSLog(NSString * _Nonnull format, ...);
-
-
+    for (NSEntityDescription *entity in self.model.entities) {
+        printf("\n Entity: %s"
+               "\n --------------------"
+               "\n Class name: %s"
+               "\n\tAttributes: "
+               ,
+               entity.name.UTF8String,
+               entity.managedObjectClassName.UTF8String);
+        for (NSPropertyDescription *attribute in entity) {
+            printf("\t\t %s", attribute.name.UTF8String);
+        }
+        printf("\n\tRelationships: ");
+        for (NSRelationshipDescription *relationship in entity.relationshipsByName) {
+            printf("\t\t %s", relationship.description.UTF8String);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
-- (void)testEntityBookNameGet{
-    NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:self.modelURL];
-    for (NSEntityDescription *entity in model.entities) {
+- (void)testEntityDescriptions {
+    //Loops through & organizes the different attributes of the bundle
+    
+    for (NSEntityDescription *entity in self.model.entities) {
         //What's the most basic way to do this?
         //Print out one line at a time
-        printf("\n");
         NSLog(@"Entities by name: %@", entity.name);
         printf("\n");
-        for (NSPropertyDescription *attribute in entity.attributesByName){
+        for (NSPropertyDescription *attribute in entity.propertiesByName) {
             NSLog(@"Attributes by name: %@", attribute);
         }
         printf("\n");
-        for (NSRelationshipDescription *relationship in entity.relationshipsByName){
+        for (NSRelationshipDescription *relationship in entity.relationshipsByName) {
             NSLog(@"Relationships by name: %@", relationship);
         }
 
-//    NSPropertyDescription *attribute = [[NSPropertyDescription alloc] description : self.name];
-    //im supposed to create a model of the entities and go into their containers and grab the name?
-
-}
+    //    NSPropertyDescription *attribute = [[NSPropertyDescription alloc] description : self.name];
+        //im supposed to create a model of the entities and go into their containers and grab the name?
+    }
     printf("\n");
-}
+    }
+
 @end
 
 // URL :
