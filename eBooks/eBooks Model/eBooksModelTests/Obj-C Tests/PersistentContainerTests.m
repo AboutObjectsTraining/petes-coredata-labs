b7b01d2edad9c968bf46f61dea94b3cfacc4fd7c
Peter Victoratos <pvictoratos@elon.edu>
2020-01-29 19:04:21 -0500

    Set up all tests for Persistent Container Lab, not understanding XCTAssert statements

--- eBooks/eBooks Model/eBooksModelTests/Obj-C Tests/PersistentContainerTests.m
+++ eBooks/eBooks Model/eBooksModelTests/Obj-C Tests/PersistentContainerTests.m
@@ -33,15 +33,41 @@
     NSString *defaultURLEnd = NSPersistentContainer.defaultDirectoryURL.lastPathComponent;
     NSString *appSupport = @"Application Support";
     
-    printf("%s", defaultURLEnd.UTF8String);
-    printf(" =? ");
-    printf("%s", appSupport.UTF8String);
+    //    printf("%s", defaultURLEnd.UTF8String);
+    //    printf(" =? ");
+    //    printf("%s", appSupport.UTF8String);
+    
+    //how to use XTCAssertEquals?
     
     if ([defaultURLEnd isEqualToString:appSupport])
-        printf("\nPassed!\n");
+        printf("\nPassed! Default URL ends with 'Application Support'\n");
     else
         printf("\nFailed!\n");
 }
+
+// Verify that the persistent store instance URL ends with the store name you provided, and its type is SQLite
+- (void) testVerifyStoreURLType {
+    //    let storeCoordinator = container.persistentStoreCoordinator
+    
+    NSPersistentStoreCoordinator *coordinator = self.container.persistentStoreCoordinator;
+    NSPersistentStore *store = coordinator.persistentStores.firstObject;
+    NSURL *name = store.URL.standardizedURL;
+    NSLog(@"%@", name);
+    //XCTAssertEqual(name, storeName);
+    //    XCTAssertEqual(store.type, "SQLite")
+}
+
+// Verify that the container's managed object model is the same that initialized the container
+- (void) testModel {
+    NSManagedObjectModel *modelFromContainer = self.container.persistentStoreCoordinator.managedObjectModel;
+    XCTAssertEqual(modelFromContainer, self.model);
+}
+
+// Verify that the container's viewContext property has an MOC with concurrency type of .mainQueueConcurrencyType
+- (void) testContext {
+    XCTAssertEqual(self.container.viewContext.concurrencyType, NSMainQueueConcurrencyType);
+}
+
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
