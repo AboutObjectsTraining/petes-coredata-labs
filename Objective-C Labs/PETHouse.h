576c284d543db525c55423871d7b889f542509d7
Peter Victoratos <pvictoratos@elon.edu>
2020-02-13 08:37:13 -0500

    added notes for something I began to implement last night that got nuked by xcode. could be cool.

---
+++ Objective-C Labs/PETHouse.h
@@ -0,0 +1,17 @@
+//
+//  PETHouse.h
+//  UnitTests
+//
+//  Created by Peter Victoratos on 2/13/20.
+//  Copyright © 2020 GGOG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PETHouse : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
