a44a7077a477a0b0799ae684bba1a60a5a4dd910
Peter Victoratos <pvictoratos@elon.edu>
2020-03-06 16:42:59 -0500

    made the animals speak. using private variables & reused some code :/ next step is to refactor

--- Barnyard/Barnyard/ViewController.m
+++ Barnyard/Barnyard/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
-
+{
+    AVAudioPlayer *_audioPlayer;
+    NSURL *_soundURL;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +21,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (IBAction)goatSpeak {
+    NSString *goatSoundPath = [NSString stringWithFormat:@"%@/goat.wav", [[NSBundle mainBundle] resourcePath]];
+    _soundURL = [NSURL fileURLWithPath:goatSoundPath];
+    
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:_soundURL error:nil];
+    [_audioPlayer play];
+}
+
+- (IBAction)roosterSpeak {
+    NSString *roosterSoundPath = [NSString stringWithFormat:@"%@/rooster.wav", [[NSBundle mainBundle] resourcePath]];
+    _soundURL = [NSURL fileURLWithPath:roosterSoundPath];
+    
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:_soundURL error:nil];
+    [_audioPlayer play];
+    
+}
+
+- (IBAction)pigSpeak {
+    NSString *pigSoundPath = [NSString stringWithFormat:@"%@/pig.wav", [[NSBundle mainBundle] resourcePath]];
+    _soundURL = [NSURL fileURLWithPath:pigSoundPath];
+    
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:_soundURL error:nil];
+    [_audioPlayer play];
 }
 
 
