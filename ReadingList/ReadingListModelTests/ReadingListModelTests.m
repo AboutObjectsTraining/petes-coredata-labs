ae3233d9ca2e854700ca0c3e1c24915f035461a6
Pete Victoratos <pete.victoratos@aboutobjects.com>
2020-03-19 13:28:13 -0400

    added some to-do's

--- ReadingList/ReadingListModelTests/ReadingListModelTests.m
+++ ReadingList/ReadingListModelTests/ReadingListModelTests.m
@@ -14,41 +14,31 @@
 
 @implementation ReadingListModelTests
 
-- (void)setUp {
-    // Create author object
-    // Access class method keys in RLTAuthor
-}
-
-- (void)testExample {
+- (void)testModelObjectDictionaries {
+    //create an array of authors and books and make some tests
     RLMAuthor *author = [[RLMAuthor alloc] initWithDictionary:@{@"firstName" : @"John", @"lastName" : @"Doe"}];
     RLMBook *book = [[RLMBook alloc] initWithDictionary:@{@"title" : @"My Average Life",
                                                           @"year"  : @"1991",
                                                           @"author": author}];
     
-    NSLog(@"%@", book.year);
     
     NSDictionary *authorDictionary = [author dictionaryRepresentation];
     NSDictionary *bookDictionary = [book dictionaryRepresentation];
-    
+    NSString *bookf = [book description];
+    NSLog(@"%@", bookf);
+
     // Get the keys for the given values and NSLog 'em
     NSLog(@"Author keys: %@ %@", authorDictionary.allKeys.firstObject, authorDictionary.allKeys.lastObject);
-    NSLog(@"Book keys: %@ %@", bookDictionary.allKeys.firstObject, bookDictionary.allKeys.lastObject);
+    NSLog(@"Book keys: %@", bookDictionary.allKeys.description);
     // Get the values for the given keys and NSLog 'em
-    NSLog(@"Author values: %@ %@", authorDictionary.allValues.firstObject, authorDictionary.allValues.lastObject);
-    NSLog(@"Book values: %@ %@", bookDictionary.allKeys.firstObject, bookDictionary.allValues.lastObject);
+    NSLog(@"Author name: %@ %@", authorDictionary.allValues.firstObject, authorDictionary.allValues.lastObject);
+    NSLog(@"Book %@: %@", bookDictionary.allKeys.firstObject, bookDictionary.allValues.firstObject);
 }
 
 // To Do:
-// Fix error "Class RLMAuthor is implemented in both"
-
-// NSDictionary *dictionary = @{
-//    @"name" : NSUserName(),
-//    @"date" : [NSDate date],
-//    @"processInfo" : [NSProcessInfo processInfo]
-//};    A dictionary literal
-
-// (NSDictionary<String *, id> *)dictionaryWithValuesForKeys:(NSArray<NSString *> *)keys;
-
-// (void)setValuesForKeysWithDictionary:(NSDictionary<NSString *, id> *)keyedValues;
+// • Fix error "Class RLMAuthor is implemented in both"
+// • Add image property (import image assets named after author's surname
+// • Stare at store controller code, write some test cases
+// • Create an array of books and two or three authors & make tests
 
 @end
