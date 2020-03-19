0c07d1a779c58ff2b79a43b43cd1c2588c4cf979
Pete Victoratos <pete.victoratos@aboutobjects.com>
2020-03-19 12:21:22 -0400

    added some basic tests

--- ReadingList/ReadingListModelTests/ReadingListModelTests.m
+++ ReadingList/ReadingListModelTests/ReadingListModelTests.m
@@ -20,12 +20,26 @@
 }
 
 - (void)testExample {
-    RLMAuthor *author = nil;
-    NSLog(@"%@", author);
+    RLMAuthor *author = [[RLMAuthor alloc] initWithDictionary:@{@"firstName" : @"John", @"lastName" : @"Doe"}];
+    RLMBook *book = [[RLMBook alloc] initWithDictionary:@{@"title" : @"My Average Life",
+                                                          @"year"  : @"1991",
+                                                          @"author": author}];
+    
+    NSLog(@"%@", book.year);
+    
+    NSDictionary *authorDictionary = [author dictionaryRepresentation];
+    NSDictionary *bookDictionary = [book dictionaryRepresentation];
+    
     // Get the keys for the given values and NSLog 'em
-//    RLTAuthor *author = [RLTModelObject dictionaryWithValuesForKeys:[RLTAuthor keys]];
+    NSLog(@"Author keys: %@ %@", authorDictionary.allKeys.firstObject, authorDictionary.allKeys.lastObject);
+    NSLog(@"Book keys: %@ %@", bookDictionary.allKeys.firstObject, bookDictionary.allKeys.lastObject);
     // Get the values for the given keys and NSLog 'em
+    NSLog(@"Author values: %@ %@", authorDictionary.allValues.firstObject, authorDictionary.allValues.lastObject);
+    NSLog(@"Book values: %@ %@", bookDictionary.allKeys.firstObject, bookDictionary.allValues.lastObject);
 }
+
+// To Do:
+// Fix error "Class RLMAuthor is implemented in both"
 
 // NSDictionary *dictionary = @{
 //    @"name" : NSUserName(),
