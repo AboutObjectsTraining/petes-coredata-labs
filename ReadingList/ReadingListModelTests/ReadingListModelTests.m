ba8611b044d5a5bae2bf2dde47ad39b246583dad
Pete Victoratos <pete.victoratos@aboutobjects.com>
2020-03-20 10:03:45 -0400

    added some more to-do's

--- ReadingList/ReadingListModelTests/ReadingListModelTests.m
+++ ReadingList/ReadingListModelTests/ReadingListModelTests.m
@@ -14,6 +14,12 @@
 
 @implementation ReadingListModelTests
 
+- (NSArray *)keyToString:(NSDictionary *)dictionary {
+    NSArray *keys = dictionary.allKeys.objectEnumerator.allObjects;
+    
+    return keys;
+}
+
 - (void)testModelObjectDictionaries {
     RLMAuthor *author = [[RLMAuthor alloc] initWithDictionary:@{@"firstName" : @"John", @"lastName" : @"Doe"}];
     RLMAuthor *author2 = [[RLMAuthor alloc] initWithDictionary:@{@"firstName" : @"Arthur", @"lastName" : @"Arthur"}];
@@ -25,9 +31,6 @@
     RLMBook *book2 = [[RLMBook alloc] initWithDictionary:@{@"title" : @"Chasing Salmon",
                                                           @"year"  : @"1919",
                                                           @"author": author2}];
-    RLMBook *book3 = [[RLMBook alloc] initWithDictionary:@{@"title" : @"My Average Life",
-                                                          @"year"  : @"1991",
-                                                          @"author": author}];
     
     //put in an rlmModelObject, get a dictionary
     NSDictionary *authorDictionary = [author dictionaryRepresentation];
@@ -36,11 +39,11 @@
     NSDictionary *bookDictionary = [book dictionaryRepresentation];
 
     NSString *bookf = [book description];
-    NSLog(@"%@", bookf);
+    NSLog(@"Description: %@", bookf);
 
     // Get the keys for the given values and NSLog 'em
     NSLog(@"Author keys: %@ %@", authorDictionary.allKeys.firstObject, authorDictionary.allKeys.lastObject);
-    NSLog(@"Book keys: %@", bookDictionary.allKeys.description);
+    NSLog(@"Book keys: %@", bookDictionary.allKeys.objectEnumerator.allObjects);
     NSLog(@"The Author: %@ wrote %@", author2Dictionary.allValues.firstObject, book2.dictionaryRepresentation);
     // Get the values for the given keys and NSLog 'em
     NSLog(@"Author name: %@ %@", authorDictionary.allValues.firstObject, authorDictionary.allValues.lastObject);
@@ -49,9 +52,11 @@
 
 // To Do:
 // • Fix error "Class RLMAuthor is implemented in both"
+// • Format nested dictionaries
 // • Make it so that there can be multiple authors
 // • Add image property (import image assets named after author's surname
 // • Stare at store controller code, write some test cases
 // • Create an array of books and two or three authors & make tests
+// • Make this test run without running the simulator
 
 @end
